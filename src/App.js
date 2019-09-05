478c168967e179af76c738ee96818b6e152968fc
kanoshinnosuke <yushph@gmail.com>
2019-09-05 11:55:03 +0800

    add Brand.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { Component, useState } from 'react';
+import Navbar from './components/navbar';
+import GlobalStyle from './styles/Global';
 
 const App = () => {
+
+    const { navbarOpen, setNavbarOpen } = useState(false);
+
+    handleNavbar = () => { // Toggle navber if it's opened or not.
+        setNavbarOpen(!navbarOpen);
+    }
+
     return (
-        <div></div>
+        <div>
+            <Navbar 
+                navbarState={navbarOpen}
+                handleNavbar={handleNavbar}
+            />
+            <GlobalStyle />
+        </div>
     )
 }
 
